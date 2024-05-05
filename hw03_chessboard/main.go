6ea2e9063a395e1ed42afb7ca960137b795970af
Lushin415 <Lushin415@gmail.com>
2024-05-05 20:56:49 +0800

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var rows, columns int
+	fmt.Print("Введите количество строк доски: ")
+	fmt.Scanln(&rows)
+	fmt.Print("Введите количество столбцов доски: ")
+	fmt.Scanln(&columns)
+
+	var board [100][100]string
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if (i+j)%2 == 0 {
+				board[i][j] = " "
+			} else {
+				board[i][j] = "#"
+			}
+		}
+	}
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			fmt.Printf("%s ", board[i][j])
+		}
+		fmt.Println()
+	}
 }
