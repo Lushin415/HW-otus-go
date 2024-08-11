7c403c441fcc75ee07c650c3c4fbd669dc4281a8
alex <Lushin415@gmail.com>
2024-08-11 08:40:59 +0800

    Линтер без ошибок

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,31 +1,28 @@
-package init
+package main
 
 import (
 	"fmt"
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+
+	"github.com/HW-otus-go/hw02_fix_app/printer"
+	"github.com/HW-otus-go/hw02_fix_app/reader"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	var path string
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
-	var err error
-	var staff []types.Employee
+	//	var err error
+	//	var staff []types.Employee
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
-
-	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
-
-	printer.PrintStaff(staff)
+	staff, err := reader.ReadJSON(path)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		printer.PrintStaff(staff)
+	}
 }
-
-// Пока еще не проходили синтаксис, ничего не понятно :(
