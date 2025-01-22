6ed5770de19da1592bc9ffa27e6e19ee8eb06db0
Lushin415 <Lushin415@gmail.com>
2025-01-22 14:23:20 +0800

    Исправления

--- hw09_serialize/tests/gob_test.go
+++ hw09_serialize/tests/gob_test.go
@@ -8,25 +8,25 @@ import (
 	"github.com/Lushin415/HW-otus-go/hw09_serialize/serializers"
 )
 
-func TestSerializationJSON(t *testing.T) {
+func TestSerializationGob(t *testing.T) {
 	// Тестовые данные
 	books := []book.Book{
 		{ID: 1, Title: "FirstBook", Author: "Alexandr", Year: 1993, Size: 350, Rate: 4.9, Sample: []byte("Образец1")},
 		{ID: 2, Title: "SecondBook", Author: "Lushin", Year: 2024, Size: 600, Rate: 2.1, Sample: []byte("Образец2")},
 	}
 
-	// JSON
-	t.Run("JSON Serialization", func(t *testing.T) {
-		data, err := serializers.SerializeToJSON(books)
+	// Gob
+	t.Run("Gob Serialization", func(t *testing.T) {
+		data, err := serializers.SerializeToGob(books)
 		if err != nil {
-			t.Fatalf("JSON serialization failed: %v", err)
+			t.Fatalf("Gob serialization failed: %v", err)
 		}
-		deserializedBooks, err := serializers.DeserializeFromJSON(data)
+		deserializedBooks, err := serializers.DeserializeFromGob(data)
 		if err != nil {
-			t.Fatalf("JSON deserialization failed: %v", err)
+			t.Fatalf("Gob deserialization failed: %v", err)
 		}
 		if !reflect.DeepEqual(books, deserializedBooks) {
-			t.Fatalf("Deserialized JSON does not match original: got %v, want %v", deserializedBooks, books)
+			t.Fatalf("Deserialized Gob does not match original: got %v, want %v", deserializedBooks, books)
 		}
 	})
 }
--- hw09_serialize/tests/msgpack_test.go
+++ hw09_serialize/tests/msgpack_test.go
@@ -8,25 +8,24 @@ import (
 	"github.com/Lushin415/HW-otus-go/hw09_serialize/serializers"
 )
 
-func TestSerializationJSON(t *testing.T) {
+func TestSerializationMsgpack(t *testing.T) {
 	// Тестовые данные
 	books := []book.Book{
 		{ID: 1, Title: "FirstBook", Author: "Alexandr", Year: 1993, Size: 350, Rate: 4.9, Sample: []byte("Образец1")},
 		{ID: 2, Title: "SecondBook", Author: "Lushin", Year: 2024, Size: 600, Rate: 2.1, Sample: []byte("Образец2")},
 	}
 
-	// JSON
-	t.Run("JSON Serialization", func(t *testing.T) {
-		data, err := serializers.SerializeToJSON(books)
+	t.Run("MessagePack Serialization", func(t *testing.T) {
+		data, err := serializers.SerializeToMessagePack(books)
 		if err != nil {
-			t.Fatalf("JSON serialization failed: %v", err)
+			t.Fatalf("MessagePack serialization failed: %v", err)
 		}
-		deserializedBooks, err := serializers.DeserializeFromJSON(data)
+		deserializedBooks, err := serializers.DeserializeFromMessagePack(data)
 		if err != nil {
-			t.Fatalf("JSON deserialization failed: %v", err)
+			t.Fatalf("MessagePack deserialization failed: %v", err)
 		}
 		if !reflect.DeepEqual(books, deserializedBooks) {
-			t.Fatalf("Deserialized JSON does not match original: got %v, want %v", deserializedBooks, books)
+			t.Fatalf("Deserialized MessagePack does not match original: got %v, want %v", deserializedBooks, books)
 		}
 	})
 }
