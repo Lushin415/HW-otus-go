975265e12b156351f9ace52b8656c05875047a77
Lushin415 <Lushin415@gmail.com>
2025-03-20 17:03:00 +0800

    ДЗ 15_доделки1

--- hw15_go_sql/internal/client/client.go
+++ hw15_go_sql/internal/client/client.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 )
 
 // RunClient запускает клиентские тесты API
@@ -29,15 +28,38 @@ func RunClient() {
 	getUsersSpendingStats()
 }
 
-// Создаем нового пользователя
+//Создаем таблицу пользователей
+
 func createUser() {
 	fmt.Println("\n--- Создание нового пользователя ---")
 
 	// Данные для создания пользователя
-	userData := map[string]string{
-		"name_user": "Новый Пользователь",
-		"email":     fmt.Sprintf("user_%d@example.com", time.Now().Unix()),
-		"password":  "simple123",
+	userData := []map[string]string{
+		{
+			"name_user": "Иван Иванов",
+			"email":     "ivan@example.com",
+			"password":  "qwerty",
+		},
+		{
+			"name_user": "Петя Смирнов",
+			"email":     "petr@example.com",
+			"password":  "123",
+		},
+		{
+			"name_user": "Женя Жбанов",
+			"email":     "jbanov@example.com",
+			"password":  "123",
+		},
+		{
+			"name_user": "Катя Жукова",
+			"email":     "jukova@example.com",
+			"password":  "456",
+		},
+		{
+			"name_user": "Аня Гавриленко",
+			"email":     "gavrilenko@example.com",
+			"password":  "456",
+		},
 	}
 
 	// Кодируем данные в JSON
@@ -55,6 +77,61 @@ func createUser() {
 
 	// Читаем ответ
 	body, _ := io.ReadAll(resp.Body)
+	fmt.Println("Ответ сервера:", string(body))
+}
+
+// Создаем список товаров
+
+func createProducts() {
+	fmt.Println("\n--- Создание новых товаров ---")
+
+	// Данные для создания товаров
+	productData := []map[string]interface{}{
+		{
+			"id_user_f": "Носки",
+			"price":     10.00,
+		},
+		{
+			"id_user_f": "Ботинки",
+			"price":     120.00,
+		},
+		{
+			"id_user_f": "Калоши",
+			"price":     50.00,
+		},
+		{
+			"id_user_f": "Сандали",
+			"price":     70.00,
+		},
+		{
+			"id_user_f": "Туфли",
+			"price":     90.00,
+		},
+	}
+
+	// Кодируем данные в JSON
+	jsonData, err := json.Marshal(productData)
+	if err != nil {
+		fmt.Println("Ошибка при кодировании данных в JSON:", err)
+		return
+	}
+
+	// Отправляем POST-запрос
+	resp, err := http.Post("http://localhost:8080/v1/product/create", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Ошибка при отправке запроса:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Читаем ответ
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Ошибка при чтении ответа:", err)
+		return
+	}
+
+	// Выводим ответ сервера
 	fmt.Println("Ответ сервера:", string(body))
 }
 
